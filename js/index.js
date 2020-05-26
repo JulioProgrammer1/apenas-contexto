7d10564eec36f42680e4aacd2cdf903715bb8a5f
JulioProgrammer1 <julioreis1000.pro@gmail.com>
2020-05-26 16:07:35 -0300

    Add comentários(javascript), comentando os códigos e mudando o fiveicon

--- js/index.js
+++ js/index.js
@@ -1,2 +1,33 @@
+//Mudando o título do <caption> dos comentários
 var titulocaption = document.querySelector("#comentarios");
 titulocaption.textContent = "Comentários";
+
+var botao = document.querySelector("#botao-add");
+//Add um escutador de eventos, click
+botao.addEventListener("click", function ( event ) {
+    event.preventDefault();
+    //Trabalhando o <form> que adiciona comentário
+    var formComentario = document.querySelector("#fromComentarios");
+    var nome = formComentario.nome.value;
+    var comentario = formComentario.comentario.value;
+
+    //Creando uma table rows <tr>
+    var comentarioTr = document.createElement("tr");
+    //Creando duas table data <td>
+    var nomeTd = document.createElement("td");
+    var comentarioTd = document.createElement("td");
+    //Pondo os valores dos <input>(s) nas tags criadas
+    nomeTd.textContent = nome;
+    comentarioTd.textContent = comentario;
+
+    //Colocando todas as table data(s) como filhas da table rows.
+    comentarioTr.appendChild(nomeTd);
+    comentarioTr.appendChild(comentarioTd);
+    //Colocando a table rows como filha da table
+    var tabelaComentarios = document.querySelector("#lista-comentarios");
+    tabelaComentarios.appendChild(comentarioTr);
+
+    //Lipando os <input>(s)
+    formComentario.nome.value = '';
+    formComentario.comentario.value = '';
+});
